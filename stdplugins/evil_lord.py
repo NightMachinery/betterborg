9083ec7754100ce412ae5e34e514913fc0ff58c2
rudi <rudiwillalwaysloveyou@gmail.com>
2018-06-08 03:29:55 +0430

    aioify

--- stdplugins/evil_lord.py
+++ stdplugins/evil_lord.py
@@ -40,6 +40,7 @@ async def get_music(name='Halsey Control',
                     cwd="./dls/" + str(uuid.uuid4()) + "/",
                     tg_event=None):
     #Be sure to set cwd again. It seems it is only set once.
+    print(name+" cwd0: \n"+cwd)
     await pexpect_ai.run('mkdir -p ' + cwd)
     child = await pexpect_ai.spawn('instantmusic', cwd=cwd)
     child.logfile = open('/tmp/mylog', 'wb')
@@ -59,8 +60,10 @@ async def get_music(name='Halsey Control',
             child.sendline('0')
     child.expect(['Download*', '\(y/n\)*'])
     child.sendline('y')
+    print(name+" cwd1: \n"+cwd)
     await (aioify(child.expect)(
         ['Fixed*', 'couldnt get album art*'], timeout=3000))
+    print(name+" cwd2: \n"+cwd)
     return cwd + str(
         await pexpect_ai.run('bash -c "ls -a | grep mp3"', cwd=cwd),
         'utf-8').strip()
