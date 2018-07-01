e2053bd8dd10341aaa64f7b57db42b9874ec87f2
rudi <rudiwillalwaysloveyou@gmail.com>
2018-07-02 01:02:49 +0430

    .

--- stdplugins/evil_lord.py
+++ stdplugins/evil_lord.py
@@ -35,6 +35,7 @@ is_interactive = True
 
 pexpect_ai = aioify(pexpect)
 os_aio = aioify(os)
+yt_aio = aioify(youtube_dl)
 
 
 ######################
@@ -176,9 +177,8 @@ async def _(event):
                         'outtmpl':
                         file_name +'%(playlist_title)s_%(title)s_%(format)s.%(ext)s'  # 'dls/%(playlist_title)s_%(title)s_%(format)s_%(autonumber)s.%(ext)s'
                     }
-                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                        ydl_aio = aioify(ydl)
-                        d2 = await ydl_aio.extract_info(url)
+                    with await youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                        d2 = await ydl.extract_info(url)
                         file_name_with_ext = file_name + (await os_aio.listdir(file_name))[0]
                         trying_to_upload_msg = await discreet_send(
                             event,
