f95ae0c7d8520b0c3e69c0055c56e56cb773e598
rudi <rudiwillalwaysloveyou@gmail.com>
2018-07-02 00:56:32 +0430

    .

--- stdplugins/evil_lord.py
+++ stdplugins/evil_lord.py
@@ -17,6 +17,7 @@ from aioify import aioify
 
 import re
 
+import asyncio
 import pexpect
 from telethon import TelegramClient, events
 from telethon.tl.functions.users import GetFullUserRequest
@@ -175,8 +176,9 @@ async def _(event):
                         'outtmpl':
                         file_name +'%(playlist_title)s_%(title)s_%(format)s.%(ext)s'  # 'dls/%(playlist_title)s_%(title)s_%(format)s_%(autonumber)s.%(ext)s'
                     }
-                    with aioify(youtube_dl.YoutubeDL(ydl_opts)) as ydl:
-                        d2 = await ydl.extract_info(url)
+                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                        ydl_aio = aioify(ydl)
+                        d2 = await ydl_aio.extract_info(url)
                         file_name_with_ext = file_name + (await os_aio.listdir(file_name))[0]
                         trying_to_upload_msg = await discreet_send(
                             event,
