6b25822f2917bf71cbd8de26fd097781d04776cf
Fereidoon Mehri <rudiwillalwaysloveyou@gmail.com>
2019-07-20 01:12:15 +0430

    .

--- stdplugins/spotify.py
+++ stdplugins/spotify.py
@@ -4,6 +4,16 @@ from IPython import embed
 
 @borg.on(events.NewMessage(pattern=r"(?i)^\.s (.*)$"))
 async def _(event):
-    if await util.isAdmin(event) and event.message.forward == None:
-        await util.aget(event, ('spotdl', '-f', '.', '-s', event.pattern_match.group(1)), False)
+    if event.message.forward == None:
+        await util.aget(event, ('spotify.zsh', event.pattern_match.group(1)), False)
+        #await util.aget(event, ('spotdl', '-f', '.', '-s', event.pattern_match.group(1)), False)
 
+@borg.on(events.NewMessage(pattern=r"(?i)^\.sb (.*)$"))
+async def _(event):
+    if event.message.forward == None:
+        await util.aget(event, ('spotify.zsh', '-b', event.pattern_match.group(1)), False)
+
+@borg.on(events.NewMessage(pattern=r"(?i)^\.sp (.*)$"))
+async def _(event):
+    if event.message.forward == None:
+        await util.aget(event, ('spotify.zsh', '-p', event.pattern_match.group(1)), False)
