bf4d97d8c5abc469e16e09ab8c501cfee7f4cd26
Fereidoon Mehri <rudiwillalwaysloveyou@gmail.com>
2019-07-27 18:39:14 +0430

    .

--- stdborg.py
+++ stdborg.py
@@ -3,11 +3,15 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import logging
-
+import os
 from uniborg import Uniborg
 
 logging.basicConfig(level=logging.INFO)
+proxy = None
+proxy_port = os.environ.get('borgp')
+if proxy_port != None:
+    proxy = (socks.SOCKS5, '127.0.0.1', int(proxy_port))
 
-borg = Uniborg("stdborg", plugin_path="stdplugins", connection_retries=None)
+borg = Uniborg("stdborg", plugin_path="stdplugins", connection_retries=None, proxy=proxy)
 
 borg.run_until_disconnected()
