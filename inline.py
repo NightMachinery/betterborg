ff9e5cbbad0939a88b94223788b78788e7771207
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-09-01 07:24:30 +0430

    Added json support to inline mode. Added gray auth to inline mode.

--- inline.py
+++ inline.py
@@ -12,10 +12,12 @@ bot.
 """
 import logging
 import os
+import json
 from os import makedirs
 from pathlib import Path
+from typing import Dict, Iterable
 from IPython import embed
-from brish import z, zp, bsh
+from brish import z, zp, bsh, zq
 from uuid import uuid4
 import re
 from cachetools import cached, LRUCache, TTLCache
@@ -36,6 +38,7 @@ logger = logging.getLogger(__name__)
 ##
 PAF = re.compile(r"(?im)^(?:\.a(n?)\s+)?((?:.|\n)*)\s+fin$")
 PDI = re.compile(r"(?im)^\.di\s+(\S+)(?:\s+(\S*))?\s+fin$")
+PC_KITSU = re.compile(r"(?im)^\.ki\s+(.+)$")
 WHITESPACE = re.compile(r"^\s*$")
 dl_base = os.getcwd() + '/dls/'
 tmp_chat = -1001496131468
@@ -53,10 +56,14 @@ def help_command(update, context):
     update.message.reply_text('Help!')
 
 
-def isAdmin(update):
+graylist = [467602588, 195391705, 92863048,
+            90821188, 915098299, 665261327, 91294899]
+
+
+def isAdmin(update, admins=[195391705]):
     res = False
     try:
-        res = update.effective_user.username in ["Arstar"]
+        res = update.effective_user.id in admins
     except:
         res = False
     return res
@@ -69,21 +76,31 @@ def inlinequery(update, context):
     ##
     # There is pretty much no info except the sender in update. context is also useless. So we can't get the replied-to file.
     ###
-    if (not isAdmin(update)):
-        return
-    def ans_text(text: str):
+    def ans_text(text: str = "", cache_time=1):  # "To undo the folded lie,"
+        if not text:
+            return
         update.inline_query.answer([InlineQueryResultArticle(
             id=uuid4(),
             title=text,
-            input_message_content=InputTextMessageContent(text, disable_web_page_preview=False))], cache_time=1, is_personal=True)
+            input_message_content=InputTextMessageContent(text, disable_web_page_preview=False))], cache_time=cache_time, is_personal=True)
+
+    if (not isAdmin(update, admins=graylist)):
+        ans_text("""Defenceless under the night
+Our world in stupor lies;
+Yet, dotted everywhere,
+Ironic points of light
+Flash out wherever the Just
+Exchange their messages:
+May I, composed like them
+Of Eros and of dust,
+Beleaguered by the same
+Negation and despair,
+Show an affirming flame.
+    - Auden""", cache_time=86400)
+        return
 
     with lock_inline:
         query = update.inline_query.query
-        if query == ".x":
-            bsh.restart()
-            cache.clear()
-            ans_text("Restarted")
-            return
         m = PDI.match(query)
         if m:
             c_id = m.group(1)
@@ -109,30 +126,60 @@ def inlinequery(update, context):
                 result = InlineQueryResultCachedMpeg4Gif(
                     id=uuid4(),
                     title=str(c_id),
-                    mpeg4_file_id=c_id) 
+                    mpeg4_file_id=c_id)
             if result:
                 try:
-                    update.inline_query.answer([result], cache_time=1, is_personal=True)
+                    update.inline_query.answer(
+                        [result], cache_time=1, is_personal=True)
                 except:
                     ans_text(traceback.format_exc())
             else:
                 ans_text(f"Invalid kind: {c_kind}")
             return
-        m = PAF.match(query)
-        if m == None:
+        m = PC_KITSU.match(query)
+        command = ''
+        cache_time = 1
+        is_personal = True
+        if m:
+            arg = zq(str(m.group(1)))
+            if not arg:
+                ans_text()
+                return
+            command = f"kitsu-getall {arg}"
+            cache_time = 86400
+            is_personal = False
+        else:
+            if (not isAdmin(update)):
+                ans_text("""The enlightenment driven away,
+The habit-forming pain,
+Mismanagement and grief:
+We must suffer them all again. - Auden""")
+                return
+            if query == ".x":
+                bsh.restart()
+                cache.clear()
+                ans_text("Restarted")
+                return
+            m = PAF.match(query)
+            if m == None:
+                ans_text()
+                return
+            command = m.group(2)
+            if m.group(1) == 'n':
+                # embed()
+                command = 'noglob ' + command
+        if not command:
+            ans_text()
             return
-        command = m.group(2)
-        if m.group(1) == 'n':
-            # embed()
-            command = 'noglob ' + command
         print(f"Inline command accepted: {command}")
         results = get_results(command)
-        update.inline_query.answer(results, cache_time=1, is_personal=True)
+        update.inline_query.answer(
+            results, cache_time=cache_time, is_personal=is_personal)
 
 
 cache = TTLCache(maxsize=256, ttl=3600)
 @cached(cache)
-def get_results(command: str):
+def get_results(command: str, json_mode: bool = True):
     cwd = dl_base + "Inline " + str(uuid4()) + '/'
     Path(cwd).mkdir(parents=True, exist_ok=True)
     res = z("""
@@ -145,29 +192,51 @@ def get_results(command: str):
     out = res.outerr
     if WHITESPACE.match(out):
         out = f"The process exited {res.retcode}."
-    results = [
-        InlineQueryResultArticle(
-            id=uuid4(),
-            title=out[:150], # Telegram truncates itself, so this is redundant.
-            input_message_content=InputTextMessageContent(out, disable_web_page_preview=False))
-    ]
-    files = list(Path(cwd).glob('*'))
-    files.sort()
-    for f in files:
-        if not f.is_dir():
-            file_add = f.absolute()
-            base_name = str(os.path.basename(file_add))
-            ext = f.suffix
-            file = open(file_add, "rb")
-            uploaded_file = updater.bot.send_document(tmp_chat, file)
-            file.close()
-            # print(f"File ID: {uploaded_file.document.file_id}")
-            results.append(
-                InlineQueryResultCachedDocument(
-                    id=uuid4(),
-                    title=base_name,
-                    document_file_id=uploaded_file.document.file_id)
-            )
+    out_j = None
+    results = []
+    try:
+        out_j = json.loads(out)
+    except:
+        pass
+    if out_j:
+        if not isinstance(out_j, str) and isinstance(out_j, Iterable):
+            for item in out_j:
+                if isinstance(item, dict):
+                    tlg_title = item.get("tlg_title", "")
+                    tlg_content = item.get("tlg_content", "")
+                    if tlg_title:
+                        results.append(
+                            InlineQueryResultArticle(
+                                id=uuid4(),
+                                title=tlg_title,
+                                input_message_content=InputTextMessageContent(tlg_content, disable_web_page_preview=False))
+                        )
+    else:
+        results = [
+            InlineQueryResultArticle(
+                id=uuid4(),
+                # Telegram truncates itself, so this is redundant.
+                title=out[:150],
+                input_message_content=InputTextMessageContent(out[:4000], disable_web_page_preview=False))
+        ]
+        files = list(Path(cwd).glob('*'))
+        files.sort()
+        for f in files:
+            if not f.is_dir():
+                file_add = f.absolute()
+                base_name = str(os.path.basename(file_add))
+                ext = f.suffix
+                file = open(file_add, "rb")
+                uploaded_file = updater.bot.send_document(tmp_chat, file)
+                file.close()
+                # print(f"File ID: {uploaded_file.document.file_id}")
+                results.append(
+                    InlineQueryResultCachedDocument(
+                        id=uuid4(),
+                        title=base_name,
+                        document_file_id=uploaded_file.document.file_id)
+                )
+
     z("command rm -r {cwd}")
     return results
 
--- stdplugins/chat_id.py
+++ stdplugins/chat_id.py
@@ -10,13 +10,15 @@ async def _(event):
 
     chat = await event.get_chat()
     await event.reply(str(chat.id))
-    
+
     r_id = event.message.reply_to_msg_id
     if r_id:
         m2 = await borg.get_messages(chat, ids=r_id)
         # embed2()
         await event.reply(f"{m2.__dict__}")
         # await event.reply(f"{m2.document.__dict__}")
+        if m2.forward:
+            await event.reply(f"{m2.forward.__dict__}")
         if m2.file:
             await event.reply(f"{m2.file.id}")
         # if m2.media:
