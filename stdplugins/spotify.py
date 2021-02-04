f9ad4792f1cbe6e00039f41b4924e1c3321fe84e
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-02-04 13:54:11 +0330

    updated .s to spotdl v3

--- stdplugins/spotify.py
+++ stdplugins/spotify.py
@@ -6,17 +6,18 @@ from IPython import embed
 @borg.on(events.NewMessage(pattern=r"(?i)^\.s (.*)$"))
 async def _(event):
     if event.message.forward == None:
-        await util.aget(event, ('spotify.zsh', event.pattern_match.group(1)), False)
+        await util.aget(event, ('spotdl', event.pattern_match.group(1)), False)
+        # await util.aget(event, ('spotify.zsh', event.pattern_match.group(1)), False)
         # await util.aget(event, ('spotdl', '-f', '.', '-s', event.pattern_match.group(1)), False)
 
 
-@borg.on(events.NewMessage(pattern=r"(?i)^\.sb+ (.*)$"))
-async def _(event):
-    if event.message.forward == None:
-        await util.aget(event, ('spotify.zsh', '-a', event.pattern_match.group(1)), False)
+# @borg.on(events.NewMessage(pattern=r"(?i)^\.sb+ (.*)$"))
+# async def _(event):
+#     if event.message.forward == None:
+#         await util.aget(event, ('spotify.zsh', '-a', event.pattern_match.group(1)), False)
 
 
-@borg.on(events.NewMessage(pattern=r"(?i)^\.sp+ (.*)$"))
-async def _(event):
-    if event.message.forward == None:
-        await util.aget(event, ('spotify.zsh', '-p', event.pattern_match.group(1)), False)
+# @borg.on(events.NewMessage(pattern=r"(?i)^\.sp+ (.*)$"))
+# async def _(event):
+#     if event.message.forward == None:
+#         await util.aget(event, ('spotify.zsh', '-p', event.pattern_match.group(1)), False)
