89630eaa7570d3ea1b75759055c5dc8c5134aeb4
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-11-22 18:05:32 +0330

    Fixed: pattern of tex2png

--- stdplugins/tex2png.py
+++ stdplugins/tex2png.py
@@ -4,10 +4,13 @@ from functools import partial
 from uniborg.util import embed2, brishz
 from brish import zs
 
-@borg.on(events.NewMessage(pattern=r"^.tex\s+(.+)$"))
+@borg.on(events.NewMessage(pattern=r"^\.tex\s+(.+)$"))
 async def _(event):
     if event.message.forward == None and event.sender:
         tex = event.pattern_match.group(1)
+        if not tex:
+            return
+
         print(
             f"tex2png (user_id={event.sender.id}, username={event.sender.username}): {tex}"
         )
