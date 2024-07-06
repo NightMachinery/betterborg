83634a73a13f9a171b0598bd9b79e2778cec3b7e
NightMachinery <feraidoonmehri@gmail.com>
2024-07-06 21:38:49 +0330

    deleter: added tqdm

--- stdplugins/deleter.py
+++ stdplugins/deleter.py
@@ -1,5 +1,5 @@
 ###
-# * usage
+# * @usage
 # ** `.del s 99999999`
 #
 # * @warning =self_only= is currently implemented as admin-only instead!
@@ -9,6 +9,7 @@ from uniborg import util
 from uniborg.util import admin_cmd, embed2
 from brish import z
 from icecream import ic
+from tqdm.asyncio import tqdm
 
 
 @borg.on(events.NewMessage(pattern=r"(?i)^\.del\s+(?P<self_only>s?)\s*(?P<n>\d+)$"))
@@ -19,14 +20,19 @@ async def _(event):
     if not (await util.isAdmin(event) and event.message.forward == None):
         # print("deleter: not admin")
         return
+
     await event.delete()
+
     n = int(event.pattern_match.group("n") or 1)
     self_only = bool(event.pattern_match.group("self_only"))
-    print(f"del received: {n}", flush=True)
+    print(f"del received: n={n}, self_only={self_only}", flush=True)
+
     chat = await event.get_chat()
     delete_count = 0
-    for msg in await borg.get_messages(chat, limit=n):
+
+    async for msg in tqdm(borg.iter_messages(chat, limit=n), total=n, desc="Deleting messages"):
         if self_only and not await util.isAdmin(None, msg=msg):
+            # print(f"skipped non-self msg: {msg}")
             continue
 
         ic(msg.raw_text)
