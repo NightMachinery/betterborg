f09ecfe82b272c6ad0a117472fa01f6d5c8625c1
NightMachinery <feraidoonmehri@gmail.com>
2025-08-02 19:26:39 +0330

    modified: todo.org; new constants.py

---
+++ uniborg/constants.py
@@ -0,0 +1,3 @@
+#: An invisible character sequence to prefix bot meta messages.
+#: This allows us to filter them out from the conversation history.
+BOT_META_INFO_PREFIX = "\u200b\u200b\u200b\u200b"
