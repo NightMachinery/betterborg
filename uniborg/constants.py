92c43e064201b61f153ab729b9617c40accffa92
NightMachinery <feraidoonmehri@gmail.com>
2025-08-17 18:00:42 +0330

    modified: constants.py

--- uniborg/constants.py
+++ uniborg/constants.py
@@ -1,3 +1,4 @@
 #: An invisible character sequence to prefix bot meta messages.
 #: This allows us to filter them out from the conversation history.
 BOT_META_INFO_PREFIX = "\u200b\u200b\u200b\u200b"
+BOT_META_INFO_LINE = f"{BOT_META_INFO_PREFIX}---{BOT_META_INFO_PREFIX}"
